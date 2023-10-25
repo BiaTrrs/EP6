2aa4f86ee51ea37c9d5643cb1be694d7cafa4ab9
bia <127901709+BiaTrrs@users.noreply.github.com>
2023-10-25 20:58:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,6 +19,12 @@ function funcao_resposta(){/* */
     var resp2 = document.querySelector('input[name="quest_2"]:checked');
     var resp3 = document.querySelector('input[name="quest_3"]:checked');
     var resp4 = document.querySelector('input[name="quest_4"]:checked');
+    var resp5 = document.querySelector('input[name="quest_5"]:checked');
+    var resp6 = document.querySelector('input[name="quest_6"]:checked');
+    var resp7 = document.querySelector('input[name="quest_7"]:checked');
+    var resp8 = document.querySelector('input[name="quest_8"]:checked');
+    var resp9 = document.querySelector('input[name="quest_9"]:checked');
+    var resp10 = document.querySelector('input[name="quest_10"]:checked');
 
     var contador = 0
 
@@ -58,16 +64,67 @@ function funcao_resposta(){/* */
         else{/* */
             document.getElementById(resp_errada_4).innerHTML = "Errada!"
         }
+
+        if(resp5.value == "correta"){
+            document.getElementById("resp_correta_5").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_5").innerHTML = "Errada!"
+        }
+
+        if(resp6.value == "correta"){
+            document.getElementById("resp_correta_6").innerHTML = "Correta!"
+            contador = contador + 1;
+
+        }
+        else{
+            document.getElementById("resp_errada_6").innerHTML = "Errada!" 
+        }
+
+        if(resp7.value == "correta"){
+            document.getElementById("resp_correta_7").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+
+        else{
+            document.getElementById("resp_errada_7").innerHTML = "Errada!"
+        }
+
+        if(resp8.value == "correta"){
+            document.getElementById("resp_correta_8").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_8").innerHTML = "Errada, uma equação de segundo grau pode ter no máximo duas raízes reais, não podendo ter 0 raízes reais ou infinitas raízes reais."
+        }
+
+        if(resp9.value == "correta"){
+            document.getElementById("resp_correta_9").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_9").innerHTML = "Errada!" 
+        }
+
+        if(resp10.value == "correta"){
+            document.getElementById("resp_correta_10").innerHTML = "Correta!"
+            contador = contador + 1;
+
+        }
+        else{
+            document.getElementById("resp_errada_10").innerHTML = "Errada!"
+        }
+
         document.getElementById("quantos_acertos").innerHTML = "Você acertou" + contador + "questões!";
     }
+
     else{/* */
         alert("não deixe questões em branco!")
     }
-    
-    
 }
 
-script type="text/javascript"/**/
+<script type="text/javascript"></script>/**/
     function mudar(objeto , imagem){
         equacao.scr = imagem
     }
