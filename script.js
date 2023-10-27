b9faa92303ad26d83b6b2eda6c4b69a9fc2d9f31
bia <127901709+BiaTrrs@users.noreply.github.com>
2023-10-27 08:47:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -26,105 +26,108 @@ function funcao_resposta(){/* */
     var resp9 = document.querySelector('input[name="quest_9"]:checked');
     var resp10 = document.querySelector('input[name="quest_10"]:checked');
 
-    var contador = 0
+    var contador = 0;
 
     if(resp1 && resp2 && resp3 && resp4 && resp5 && resp6 && resp7 && resp8 && resp9 && resp10){/* */
 
         if(resp1.value == "correta"){/* */
-            document.getElementById(resp_correta_1).innerHTML = "Correta!" 
+            document.getElementById("resp_correta_1").innerHTML = "Correta!";
             contador = +1;
         }
 
         else{/* */
-            document.getElementById(resp_errada_1).innerHTML = "Errada!"
+            document.getElementById("resp_errada_1").innerHTML = "Errada!";
         }
 
         if(resp2.value == "correta"){/* */
-            document.getElementById(resp_correta_2).innerHTMl = "Correta!"
+            document.getElementById("resp_correta_2").innerHTMl = "Correta!";
             contador = +1;
         }
 
         else{/* */
-            document.getElementById(resp_errada_2).innerHTML = "Errada!"
+            document.getElementById("resp_errada_2").innerHTML = "Errada!";
         }
 
         if(resp3.value == "Correta!"){/* */
-            document.getElementById(resp_correta_3).innerHTML = "Correta!"
+            document.getElementById("resp_correta_3").innerHTML = "Correta!";
             contador = +1;
         }
     
         else{/* */
-            document.getElementById(resp_errada_3).innerHTML = "Errada!"
+            document.getElementById("resp_errada_3").innerHTML = "Errada!";
         }
 
         if(resp4.value == "Correta!"){/* */
-            document.getElementById(resp_correta_4).innerHTML = "Correta!"
+            document.getElementById("resp_correta_4").innerHTML = "Correta!";
             contador = +1;
         }
+
         else{/* */
-            document.getElementById(resp_errada_4).innerHTML = "Errada!"
+            document.getElementById("resp_errada_4").innerHTML = "Errada!";
         }
 
         if(resp5.value == "correta"){
-            document.getElementById("resp_correta_5").innerHTML = "Correta!"
+            document.getElementById("resp_correta_5").innerHTML = "Correta!";
             contador = contador + 1;
         }
+
         else{
-            document.getElementById("resp_errada_5").innerHTML = "Errada!"
+            document.getElementById("resp_errada_5").innerHTML = "Errada!";
         }
 
         if(resp6.value == "correta"){
-            document.getElementById("resp_correta_6").innerHTML = "Correta!"
+            document.getElementById("resp_correta_6").innerHTML = "Correta!";
             contador = contador + 1;
 
         }
         else{
-            document.getElementById("resp_errada_6").innerHTML = "Errada!" 
+            document.getElementById("resp_errada_6").innerHTML = "Errada!"; 
         }
 
         if(resp7.value == "correta"){
-            document.getElementById("resp_correta_7").innerHTML = "Correta!"
+            document.getElementById("resp_correta_7").innerHTML = "Correta!";
             contador = contador + 1;
         }
 
         else{
-            document.getElementById("resp_errada_7").innerHTML = "Errada!"
+            document.getElementById("resp_errada_7").innerHTML = "Errada!";
         }
 
         if(resp8.value == "correta"){
-            document.getElementById("resp_correta_8").innerHTML = "Correta!"
+            document.getElementById("resp_correta_8").innerHTML = "Correta!";
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_8").innerHTML = "Errada!"
+            document.getElementById("resp_errada_8").innerHTML = "Errada!";
         }
 
         if(resp9.value == "correta"){
-            document.getElementById("resp_correta_9").innerHTML = "Correta!"
+            document.getElementById("resp_correta_9").innerHTML = "Correta!";
             contador = contador + 1;
         }
+
         else{
-            document.getElementById("resp_errada_9").innerHTML = "Errada!" 
+            document.getElementById("resp_errada_9").innerHTML = "Errada!"; 
         }
 
         if(resp10.value == "correta"){
-            document.getElementById("resp_correta_10").innerHTML = "Correta!"
+            document.getElementById("resp_correta_10").innerHTML = "Correta!";
             contador = contador + 1;
-
         }
+
         else{
-            document.getElementById("resp_errada_10").innerHTML = "Errada!"
+            document.getElementById("resp_errada_10").innerHTML = "Errada!";
         }
 
-        document.getElementById("quantos_acertos").innerHTML = "Você acertou" + contador + "questões!";
+        document.getElementById("quantos_acertos").innerHTML = " Você acertou " + contador + " questões! ";
     }
 
     else{/* */
-        alert("não deixe questões em branco!")
+        alert("não deixe questões em branco!");
     }
+
 }
 
-<script type="text/javascript"></script>/**/
     function mudar(objeto , imagem){
         equacao.scr = imagem
     }
